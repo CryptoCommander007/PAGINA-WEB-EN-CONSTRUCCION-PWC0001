5f00db55ccc5cac9976adef3361b6204e4916c3b
CryptoCommander007 <rivantped@gmail.com>
2024-05-20 12:43:34 -0400

    PAGINA-WEB-EN-CONSTRUCCION-PWC0001

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "PAGINA-WEB-EN-CONSTRUCCION-PWC0001"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/PAGINA-WEB-EN-CONSTRUCCION-PWC0001.git
+git push -u origin main
